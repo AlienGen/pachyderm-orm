27599eb7f94ba0faaac293c5103f8654432ca24e
Timothé Mermet-Buffet <tim@aliengen.com>
2022-04-24 18:07:57 +0800

    Fix callable in the query builder.

--- src/Pachyderm/Orm/QueryBuilder.php
+++ src/Pachyderm/Orm/QueryBuilder.php
@@ -2,46 +2,50 @@
 
 namespace Pachyderm\Orm;
 
-class QueryBuilder {
+class QueryBuilder
+{
 
     protected $model = NULL;
     protected $filters = array();
 
-    public function __construct($serialized = NULL) {
-        if($serialized !== NULL) {
+    public function __construct($serialized = NULL)
+    {
+        if ($serialized !== NULL) {
             $this->filters = (array)json_decode(base64_decode($serialized), TRUE);
         }
     }
 
-    private function append($operator, $filter) {
-        if(empty($this->filters)) {
+    private function append($operator, $filter)
+    {
+        if (empty($this->filters)) {
             $this->filters = $filter;
             return $this;
         }
         $this->filters = [$operator => [$this->filters, $filter]];
     }
 
-    private function addWhere($globalOperator, $field, $operator, $value) {
-        if(empty($field)) {
+    private function addWhere($globalOperator, $field, $operator, $value)
+    {
+        if (empty($field)) {
             return $this;
         }
 
-        if(is_array($field)) {
+        if (is_array($field)) {
             $this->append($globalOperator, $field);
             return $this;
         }
 
-        if($field instanceof QueryBuilder) {
+        if ($field instanceof QueryBuilder) {
             $this->append($globalOperator, $field->build());
             return $this;
         }
 
-        if(is_callable($field)) {
+        if (is_callable([$this, $field])) {
             $field($this);
             return $this;
         }
 
-        if($value === NULL && $operator !== NULL) {
+        if ($value === NULL && $operator !== NULL) {
             $this->append($globalOperator, [$operator => [$field]]);
             return $this;
         }
@@ -51,27 +55,33 @@ class QueryBuilder {
         return $this;
     }
 
-    public function where($field, $operator = NULL, $value = NULL) {
+    public function where($field, $operator = NULL, $value = NULL)
+    {
         return $this->addwhere('AND', $field, $operator, $value);
     }
 
-    public function orWhere($field, $operator = NULL, $value = NULL) {
+    public function orWhere($field, $operator = NULL, $value = NULL)
+    {
         return $this->addwhere('OR', $field, $operator, $value);
     }
 
-    public function build() {
+    public function build()
+    {
         return $this->filters;
     }
 
-    public function serialize() {
+    public function serialize()
+    {
         return base64_encode(json_encode($this->filters));
     }
 
-    public function setModel($classname) {
+    public function setModel($classname)
+    {
         $this->model = $classname;
     }
 
-    public function get() {
+    public function get()
+    {
         $model = $this->model;
         // @FIXME: Remove offset and limit
         return $model::findAll($this, NULL, 0, 1000000);
