d3cd4120a2a5d6b498886260891de53f1f25a06c
Timothé Mermet-Buffet <tim@aliengen.com>
2022-10-29 18:16:03 +0800

    Use the iterable object set.

--- src/Pachyderm/Orm/AbstractModel.php
+++ src/Pachyderm/Orm/AbstractModel.php
@@ -2,72 +2,15 @@
 
 namespace Pachyderm\Orm;
 
-abstract class AbstractModel implements \ArrayAccess
+use Pachyderm\Utils\IterableObjectSet;
+
+abstract class AbstractModel extends IterableObjectSet
 {
     public null|string $deleted_at = null;
     public null|string $updated_at = null;
     public null|string $created_at = null;
     protected array $_fields = array();
     protected array $_data = array();
-
-    /**
-     * Magic getter to get the value of a field.
-     */
-    public function __get($field)
-    {
-        if (!isset($this->_data[$field])) {
-            return null;
-        }
-        return $this->_data[$field];
-    }
-
-    /**
-     * Magic setter to set the value of a field
-     */
-    public function __set($field, $value)
-    {
-        $this->_data[$field] = $value;
-    }
-
-    /**
-     * Magic isset to define the behavior of the isset method.
-     */
-    public function __isset(string $field): bool
-    {
-        return isset($this->_data[$field]);
-    }
-
-    /**
-     * Array like setter
-     */
-    public function offsetSet($key, $value): void
-    {
-        $this->_data[$key] = $value;
-    }
-
-    /**
-     * Array like exists
-     */
-    public function offsetExists($key): bool
-    {
-        return array_key_exists($key, $this->_data);
-    }
-
-    /**
-     * Array like unset
-     */
-    public function offsetUnset($key): void
-    {
-        unset($this->_data[$key]);
-    }
-
-    /**
-     * Array like getter
-     */
-    public function offsetGet($key): mixed
-    {
-        return $this->_data[$key];
-    }
 
     /**
      * Account constructor.
@@ -119,6 +62,9 @@ abstract class AbstractModel implements \ArrayAccess
         return $this->_data[$this->primary_key];
     }
 
+    /**
+     * @deprecated The model is iterable.
+     */
     public function toArray(): array
     {
         return $this->_data;
--- src/Pachyderm/Orm/Collection.php
+++ src/Pachyderm/Orm/Collection.php
@@ -2,144 +2,107 @@
 
 namespace Pachyderm\Orm;
 
-class Collection implements \Iterator, \Countable, \ArrayAccess
+use Pachyderm\Utils\IterableObjectSet;
+
+class Collection extends IterableObjectSet
 {
-  protected $_data = array();
-  protected int $_total_record = 0;
+    protected int $_total_record = 0;
 
-  public function __construct(int $totalRecords = 0)
-  {
-    $this->_total_record = $totalRecords;
-  }
-
-  public function totalRecords(): int
-  {
-    return $this->_total_record;
-  }
-
-  /**
-   * Array like setter
-   */
-  public function offsetSet($key, $value): void
-  {
-    if (!$value instanceof AbstractModel) {
-      throw new \Error('Unable to add other type to a collection!');
+    public function __construct(int $totalRecords = 0)
+    {
+        $this->_total_record = $totalRecords;
     }
-    $this->_data[$key] = $value;
-  }
 
-  /**
-   * Array like exists
-   */
-  public function offsetExists($key): bool
-  {
-    return array_key_exists($key, $this->_data);
-  }
-
-  /**
-   * Array like unset
-   */
-  public function offsetUnset($key): void
-  {
-    unset($this->_data[$key]);
-  }
-
-  /**
-   * Array like getter
-   */
-  public function offsetGet($key): mixed
-  {
-    return $this->_data[$key];
-  }
-
-  public function add(AbstractModel $m): void
-  {
-    $this->_data[] = $m;
-  }
-
-  public function addAll(Collection $c): void
-  {
-    foreach ($c as $m) {
-      $this->add($m);
+    public function totalRecords(): int
+    {
+        return $this->_total_record;
     }
-  }
 
-  public function first(): null|AbstractModel
-  {
-    if (empty($this->_data[0])) {
-      return null;
+    /**
+     * Avoid the modification of the collection using object access.
+     */
+    public function __set($key, $value)
+    {
+        throw new \Error('Collection cannot contains new properties!');
     }
-    return $this->_data[0];
-  }
 
-  public function count(): int
-  {
-    return count($this->_data);
-  }
-
-  public function current(): null|AbstractModel
-  {
-    return current($this->_data);
-  }
-
-  public function next(): void
-  {
-    next($this->_data);
-  }
-
-  public function key(): null|string|int
-  {
-    return key($this->_data);
-  }
-
-  public function valid(): bool
-  {
-    return current($this->_data) != NULL;
-  }
-
-  public function rewind(): void
-  {
-    reset($this->_data);
-  }
-
-  /**
-   * @deprecated
-   */
-  public function toArray(): array
-  {
-    $array = array();
-    foreach ($this->_data as $m) {
-      $array[] = $m->toArray();
+    /**
+     * Ensure the type of data stored in the collection.
+     */
+    public function offsetSet($key, $value): void
+    {
+        if (!$value instanceof AbstractModel) {
+            throw new \Error('Unable to add other type to a collection!');
+        }
+        $this->_data[$key] = $value;
     }
-    return $array;
-  }
 
-  /**
-   * @deprecated
-   */
-  public function toObject(string $pk = null): iterable
-  {
-    $object = array();
-    foreach ($this->_data as $m) {
-      if (empty($pk)) {
-        $pk = $m->primary_key;
-      }
-      $object[$m->$pk] = $m->toArray();
+    public function add(AbstractModel $m): void
+    {
+        $this->_data[] = $m;
     }
-    return $object;
-  }
 
-  public function save(): void
-  {
-    foreach ($this->_data as $m) {
-      $m->save();
+    public function addAll(Collection $c): void
+    {
+        foreach ($c as $m) {
+            $this->add($m);
+        }
     }
-  }
 
-  public function delete(): void
-  {
-    foreach ($this->_data as $m) {
-      $m->delete();
+    public function first(): null|AbstractModel
+    {
+        if (empty($this->_data[0])) {
+            return null;
+        }
+        return $this->_data[0];
     }
-  }
+
+    /**
+     * Ensure the type returned by the collection.
+     */
+    public function current(): null|AbstractModel
+    {
+        return current($this->_data);
+    }
+
+    /**
+     * @deprecated The collection is iterable
+     */
+    public function toArray(): array
+    {
+        $array = array();
+        foreach ($this->_data as $m) {
+            $array[] = $m->toArray();
+        }
+        return $array;
+    }
+
+    /**
+     * @deprecated
+     */
+    public function toObject(string $pk = null): iterable
+    {
+        $object = array();
+        foreach ($this->_data as $m) {
+            if (empty($pk)) {
+                $pk = $m->primary_key;
+            }
+            $object[$m->$pk] = $m->toArray();
+        }
+        return $object;
+    }
+
+    public function save(): void
+    {
+        foreach ($this->_data as $m) {
+            $m->save();
+        }
+    }
+
+    public function delete(): void
+    {
+        foreach ($this->_data as $m) {
+            $m->delete();
+        }
+    }
 }
